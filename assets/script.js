c701fbb6f14b44eae76ec6faaa157e8033e5ccab
Donniecia  Corke <donnieciamc@gmail.com>
2022-07-19 11:13:08 -0400

    including comments in the script file with logic of how to set up the page

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//variables to be used throughout script file//
+//form submission conditions. Entering city name, if no name entered alert to Enter a city//
+//storing searched cities//
+//utiizing api key and openweather site to fetch the weather//
+//clearing old content so that a new search is successful//
+//using moment to set the date//
+//adding the weather icons to reflect the current weather conditions
