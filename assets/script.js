35a647b36970536cf68e3bac697939f0c0cc4f64
Donniecia  Corke <donnieciamc@gmail.com>
2022-07-19 11:20:25 -0400

    addition of variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,13 @@
 //variables to be used throughout script file//
+var cities = [];
+var cityFormEl = document.querySelector("#search-city");
+var cityInputEl = document.querySelector("#city");
+var pastSearchButtonEl = document.querySelector("#previous-search-button");
+var forecastTitle = document.querySelector("#forecast");
+var weatherContainerEl = document.querySelector("#current-weather-contianer");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastContainerEl = document.querySelector("#fiveday-contianer");
+
 //form submission conditions. Entering city name, if no name entered alert to Enter a city//
 //storing searched cities//
 //utiizing api key and openweather site to fetch the weather//
